23c21b39cf5dae7655a0887618147258d3c2b3bf
Vikash Basant <basantvikash360@gmail.com>
2023-07-04 11:11:39 +0530

    now spring is creating object for us using beans.xml where we need to specified the class which we want create an object

--- src/main/java/com/sprincioc/ioc/MobileUserApplication.java
+++ src/main/java/com/sprincioc/ioc/MobileUserApplication.java
@@ -1,19 +1,13 @@
 package com.sprincioc.ioc;
 
-import com.sprincioc.ioc.service.AirtelService;
-import com.sprincioc.ioc.service.JioService;
-import com.sprincioc.ioc.service.VodaPhoneService;
-import com.sprincioc.ioc.uitl.SimService;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MobileUserApplication {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-
-        // Creating an object of VodaPhoneService Class and store into SimService Interface Reference and then call
-        // the functionality of Voda-Phone Class:
-        SimService simService = new VodaPhoneService();
-        simService.calling();
-        simService.data();
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        System.out.println("config loaded!");
     }
 }
-
