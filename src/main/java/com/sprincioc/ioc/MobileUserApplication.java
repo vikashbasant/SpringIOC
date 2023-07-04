55a23fe52b0a52039a769bc3255ea74afff65ed8
Vikash Basant <basantvikash360@gmail.com>
2023-07-04 10:06:56 +0530

    now we are using SimService interface reference to store object anyService class, so now if we want change the sim create new className();

--- src/main/java/com/sprincioc/ioc/MobileUserApplication.java
+++ src/main/java/com/sprincioc/ioc/MobileUserApplication.java
@@ -3,15 +3,17 @@ package com.sprincioc.ioc;
 import com.sprincioc.ioc.service.AirtelService;
 import com.sprincioc.ioc.service.JioService;
 import com.sprincioc.ioc.service.VodaPhoneService;
+import com.sprincioc.ioc.uitl.SimService;
 
 public class MobileUserApplication {
     public static void main(String[] args){
 
 
-        // Creating an object of VodaPhoneService Class and use it manually:
-        VodaPhoneService vodaPhone = new VodaPhoneService();
-        vodaPhone.calling();
-        vodaPhone.data();
+        // Creating an object of VodaPhoneService Class and store into SimService Interface Reference and then call
+        // the functionality of Voda-Phone Class:
+        SimService simService = new VodaPhoneService();
+        simService.calling();
+        simService.data();
     }
 }
 
