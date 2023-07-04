9cc6babf94268ec4557113e7b83f5623d87ba0e6
Vikash Basant <basantvikash360@gmail.com>
2023-07-04 11:34:17 +0530

    Now our application should configuable and don't touch our source code!

--- src/main/java/com/sprincioc/ioc/MobileUserApplication.java
+++ src/main/java/com/sprincioc/ioc/MobileUserApplication.java
@@ -1,7 +1,7 @@
 package com.sprincioc.ioc;
 
 
-import com.sprincioc.ioc.service.VodaPhoneService;
+import com.sprincioc.ioc.uitl.SimService;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -12,14 +12,16 @@ public class MobileUserApplication {
         // Creating an object using beans.xml file
         // inside the beans.xml file, we need to specify the class which we want an object:
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        System.out.println("config loaded!");
+        System.out.println("=>>config loaded!<<=");
 
+        // =>> Generalized the use of SimService Interface reference:
         // Now we need to use an object which is created by spring IOC:
         // .getBean() method, return an object type Object:
-        // Without type casting to VodaPhoneService:
-        VodaPhoneService voda = context.getBean("voda", VodaPhoneService.class);
-        // calling VodaService functionality:
-        voda.calling();
-        voda.data();
+        // here we are using compile time polymorphism:
+        // SimService Interface reference variable store an object:
+        SimService sim = context.getBean("sim", SimService.class);
+        // calling functionality:
+        sim.calling();
+        sim.data();
     }
 }
